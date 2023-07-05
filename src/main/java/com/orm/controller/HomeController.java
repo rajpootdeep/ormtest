bd762f3dafdd4416cda39b945a8dbfe1640a4ab9
rajpootdeep <deepnarayan.kudra@gmail.com>
2023-07-05 16:58:10 +0530

    First Commit

---
+++ src/main/java/com/orm/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.orm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.orm.model.Alien;
+
+@Controller
+public class HomeController {
+
+	@ModelAttribute
+	public void modelData(Model m) {
+		m.addAttribute("name", "Deepu");
+	}
+//	@RequestMapping("/")
+//	public String home() {
+//		return "index";
+//	}
+
+	// @RequestMapping("/addAlien")
+//	public String addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, Model m) {
+//
+//		Alien a = new Alien();
+//		a.setAid(aid);
+//		a.setAname(aname);
+//		m.addAttribute("Alien", a);
+//		return "result";
+//	}
+
+//	public String addAlien(@ModelAttribute Alien a, Model m) {
+//		m.addAttribute("Alien", a);
+//		return "result";
+//	}
+	@PostMapping("addAlien")
+	public String addAlien(@ModelAttribute("a1") Alien a) {
+
+		return "result";
+	}
+
+	// @GetMapping("getAlien")
+//	public String getAlien(Model m) {
+//		List<Alien> aliens = Arrays.asList(new Alien(12, "Deep Kumar"));
+//		m.addAttribute("result", aliens);
+//		return "showalien";
+//	}
+
+	@GetMapping("getAlien")
+	public String getAlien(Model m) {
+
+		// m.addAttribute("result", dao.getAliens());
+		return "showalien";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/result.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page isELIgnored="false"%>
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Alien Details...</title>
+</head>
+<body>
+<%--Result is : ${Alien} --%>
+Result is : ${a1}
+
+Welcome Aliens by ${name}
+
+</body>
+</html>
